9fb87bea82ae38faf325df19d46de671a6fb3208
Hans Marasigan <hansmarasigan@gmail.com>
2016-06-20 14:53:45 -0700

    Create aboutme.js

---
+++ aboutme.js
@@ -0,0 +1,41 @@
+/*java script page!*/
+
+/* var clickerButton = document.querySelectorAll(".button");
+
+var onButtonClick=function(){
+	var bodyColor=document.getElementByTagName("body");
+	bodyColor.style.background=red;
+}
+
+clickerButton.addEventListener("click", onButtonClick);
+
+var bluered= document.getElementByTagName("body");
+bluered.style.background="red";
+ */
+ 
+ 
+
+var allButtons = document.getElementsByTagName("button");
+
+var bodyEl= document.getElementsByClassName("rButton");
+
+function getAllButtons(){
+	 //var allButtons = document.getElementsByTagName("p");
+	
+	
+	var num = allButtons.length;
+	
+	console.log(bodyEl);
+		for (i=0;i<bodyEl.length;i++)
+		{
+		bodyEl[i].style.backgroundColor= "red";
+		} 
+	 alert("there are " + num + "buttons");
+ }
+
+ function addFun(){
+	 allButtons.onclick=getAllButtons();
+ }
+ 
+ 
+ 
